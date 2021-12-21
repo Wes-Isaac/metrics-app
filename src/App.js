2329128aa571f49a58e92595a3b6472d62c8ee70
unknown <yisehakmitiku@gmail.com>
2021-12-21 16:58:31 +0800

    Setup routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Detail from './components/Detail';
+import Home from './components/Home';
+
 function App() {
   return (
-    <div className="App">
-      <h2>HELLO</h2>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/detail" element={<Detail />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Home() {
+  return (
+    <div>
+      <h2>HOME</h2>
+      <Link to="/detail">Detail</Link>
+      <Link to="/detail">Detail</Link>
+      <Link to="/detail">Detail</Link>
+      <Link to="/detail">Detail</Link>
+      <Link to="/detail">Detail</Link>
+      <Link to="/detail">Detail</Link>
+      <Link to="/detail">Detail</Link>
+    </div>
+  );
+}
