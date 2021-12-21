db17dbeb6f0f5c994dbe6190cf3e491002c97929
unknown <yisehakmitiku@gmail.com>
2021-12-21 17:07:19 +0800

    Create cofigureStore

---
+++ src/redux/configureStore.js
@@ -0,0 +1,16 @@
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+
+
+const reducer = combineReducers({
+  home: homeReducer,
+  detail: detailReducer,
+});
+
+const store = createStore(
+  reducer,
+  applyMiddleware(thunk, logger),
+);
+
+export default store;
